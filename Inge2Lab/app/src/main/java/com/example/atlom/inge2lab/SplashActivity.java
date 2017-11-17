366283542d402377b61521782a39fa6451186fac
Atlom <jaaf27@hotmail.com>
2017-11-17 16:43:27 -0300

    Splash Screen agregada

---
+++ Inge2Lab/app/src/main/java/com/example/atlom/inge2lab/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.atlom.inge2lab;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+    private final int DURACION_SPLASH = 2000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.
+                LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(SplashActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DURACION_SPLASH);
+    }
+}
